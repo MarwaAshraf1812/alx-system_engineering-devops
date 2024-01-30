17920ef29b73e9272474ef0a58c8797ba289da17
MarwaAshraf1812 <marashraf090@gmail.com>
2024-01-30 14:37:47 +0200

    Task 1: Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,11 @@
-#!/usr/bin/pup
-# Install a version of flask (2.1.0)
+# installs the flask package 
 
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3'
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command  => '/usr/bin/pip3 install Flask==2.1.0 Werkzeug==2.1.1',
+  provider => 'shell',
+  require  => Package['python3-pip'],
 }
