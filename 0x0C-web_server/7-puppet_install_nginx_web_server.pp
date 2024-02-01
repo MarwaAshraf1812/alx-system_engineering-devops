4aded84a065d376b811dd9b0bea59bd980bc9ad9
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-01 21:25:29 +0200

    Task 5: Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,24 +1,39 @@
-# Setup New Ubuntu server with nginx
+#install and configure an Nginx server using Puppet instead of Bash
 
-exec {'update system':
-    command => '/usr/bin/apt-get update',
+exec { 'update_system':
+  command => '/usr/bin/apt-get update',
 }
 
-package {'nginx':
-    ensure => 'installed,
-    require => Exec['update system']
+package { 'nginx':
+  ensure  => 'installed',
+  require => Exec['update_system'],
 }
 
-file{'/var/www/html/index.html':
-    content => 'Hello World!'
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => template('nginx/default.erb'),
+  require => Package['nginx'],
 }
 
-exec {'redirect_me':
-    command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
+file { '/etc/nginx/sites-enabled/default':
+  ensure  => link,
+  target  => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
 }
 
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
+file { '/var/www/html/index.html':
+  ensure  => file,
+  content => 'Hello World!',
+  require => Package['nginx'],
+}
+
+exec { 'add_rewrite_rule':
+  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  provider => 'shell',
+  require  => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => [File['/etc/nginx/sites-enabled/default'], Exec['add_rewrite_rule']],
 }
