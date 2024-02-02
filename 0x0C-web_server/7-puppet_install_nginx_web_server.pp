17df60759d7669f9fffadbe58d815f1f59adf68a
MarwaAshraf1812 <marashraf090@gmail.com>
2024-02-02 02:56:21 +0200

    PP

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,39 +1,26 @@
-#install and configure an Nginx server using Puppet instead of Bash
+# Setup New Ubuntu server with nginx
 
-exec { 'update_system':
+exec { 'update system':
   command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
   ensure  => 'installed',
-  require => Exec['update_system'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => template('nginx/default.erb'),
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-enabled/default':
-  ensure  => link,
-  target  => '/etc/nginx/sites-available/default',
-  require => File['/etc/nginx/sites-available/default'],
+  require => Exec['update system'],
 }
 
 file { '/var/www/html/index.html':
   ensure  => file,
   content => 'Hello World!',
-  require => Package['nginx'],
 }
 
-exec { 'add_rewrite_rule':
-  command  => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-  provider => 'shell',
-  require  => Package['nginx'],
+exec { 'redirect_me':
+  command => 'sed -i "24i\ rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+  require => Package['nginx'],
+  path    => ['/bin', '/usr/bin'],
 }
 
 service { 'nginx':
-  ensure  => running,
-  require => [File['/etc/nginx/sites-enabled/default'], Exec['add_rewrite_rule']],
+  ensure  => 'running',
+  require => Package['nginx'],
 }
