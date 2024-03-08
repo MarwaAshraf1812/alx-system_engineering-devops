8f20ed7f479b33bb5b02b3f56abfd5b60297a429
MarwaAshraf1812 <marashraf090@gmail.com>
2024-03-08 13:09:36 +0200

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,13 +6,25 @@ def number_of_subscribers(subreddit):
     """Get the number of subscribers for a given subreddit."""
     import requests
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
 
     # Set a custom User-Agent header to avoid Too Many Requests issues
     headers = {"User-Agent": "My-User-Agent"}
 
-    sub_info = requests.get(url, headers=headers, allow_redirects=False)
-    if sub_info.status_code >= 300:
+    try:
+        sub_info = requests.get(url, headers=headers, allow_redirects=False)
+
+        if sub_info.status_code >= 300:
+            return 0
+
+        # Parse the JSON response and extract the number of subscribers
+        data = sub_info.json().get("data")
+        if data and "subscribers" in data:
+            return data["subscribers"]
+        else:
+            # Handle the case where the JSON structure is unexpected
+            return 0
+
+    except requests.RequestException:
+        # Handle any exception that might occur during the request
         return 0
-    # Parse the JSON response and extract the number of subscribers
-    return sub_info.json().get("data").get("subscribers")
